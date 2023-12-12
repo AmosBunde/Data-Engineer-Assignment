c2e8f5451a636a658ca2b72ab698def3b5bb5914
BundeAmos <git.bunde@gmail.com>
2023-12-12 15:47:31 +0300

    Creatting temporary view in Spark, Transformation Joins from other dimension tables

--- etl_increload.py
+++ etl_increload.py
@@ -1,7 +1,10 @@
-import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import lit, col, when, coalesce, udf
-from pyspark.sql.types import StructType, StringType, IntegerType, DateType
+from pyspark.sql.functions import col
+from pyspark.sql.window import Window
+from pyspark.sql import functions as F
+
+# Specify CSV path for Pupil_details
+pupil_details_path = "C:\Users\ARCHITECT\Documents\Data-Engineer-Assignment\pupils_details.csv"
 
 
 #Create a Spark session
@@ -24,5 +27,30 @@ stream_df = spark.read.jdbc(jdbc_url, "Stream", properties = connection_properti
 attendance_df = spark.read.jdbc(jdbc_url, "Attendance", properties = connection_properties)
 grades_df = spark.read.jdbc(jdbc_url, "Grades", properties = connection_properties)
 status_df = spark.read.jdbc(jdbc_url, "Status", properties = connection_properties)
+
+#Create or replace temporary views for tables
+academy_df.createOrReplaceTempView("Academy")
+stream_df.createOrReplaceTempView("Stream")
+attendance_df.createOrReplaceTempView("Attendance")
+grades_df.createOrReplaceTempView("Grades")
+status_df.createOrReplacetempView("Status")
+
+# Fetch data from the csv for pupils.
+pupil_Details_df = spark.read.option("header","true").csv(pupil_details_path)
+
+
+#Extract data based on Snapshot of the csv
+latest_snapshot = pupil_Details_df.agg({"SnapshotDate": "max"}).collect()[0][0]
+new_pupil_data_df = pupil_Details_df.filter(f"snapshotDate > '{latest_snapshot}'")
+
+
+#Performing transformation by joining with other tables
+
+new_pupil_data_df = new_pupil_data_df.join(academy_df, on="AcademyName", how="left")
+new_pupil_data_df = new_pupil_data_df.join(stream_df, on="Stream", how="left")
+new_pupil_data_df = new_pupil_data_df.join(grades_df, on="GradeName", how="left")
+new_pupil_data_df = new_pupil_data_df.join(attendance_df, on="Attendance", how="left")
+new_pupil_data_df = new_pupil_data_df.join(status_df, on="Status", how="left")
+
 
 
